56b10492b912dfb07f901c0f9cf6eb41310546bb
Konrad Bielak <konrad.bielak@gmail.com>
2017-01-10 01:07:37 +0100

    Simplistic drag n drop

--- src/app/elements-container.component.ts
+++ src/app/elements-container.component.ts
@@ -1,20 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-elements-container',
-  template: `<div (drop)="onDrop($event)" (dragover)="allowDrop($event)"></div>`,
-  styles: [ `div {position: absolute; width: 100%; height: 100%}` ]
+  template: `<div (dragenter)="onDragEnter($event)" (dragover)="onDragMove($event)" (dragleave)="onDragLeave($event)" (drop)="onDragFinished($event)">
+               <div #test id="test">Test</div>
+             </div>`,
+  styles: [ `div {position: absolute; width: 100%; height: 100%}
+             #test { display: none; width: 100px; height: 20px; border: 1px solid red; position: absolute; left: 10px; top: 10px; pointer-events: none; }` ]
 })
 export class ElementsContainerComponent {
-  
-  onDrop(event) {
-    let dataTransfer = event.dataTransfer.getData('data');
-    console.log('onDrop', dataTransfer)
+
+  @ViewChild('test')
+  testElementRef: ElementRef;
+
+  onDragEnter(event: DragEvent) {
+    this.testElementRef.nativeElement.style.display = 'block';
+  }
+
+  onDragMove(event: DragEvent) {
+    const element = this.testElementRef.nativeElement;
+    let x = event.offsetX - (element.width|element.clientWidth)/2;
+    let y = event.offsetY - (element.height|element.clientHeight)/2;
+
+    if (!event.altKey) {
+      x = Math.round(x/10)*10;
+      y = Math.round(y/10)*10;
+    }
+
+    element.style.left = x + 'px';
+    element.style.top = y + 'px';
     event.preventDefault();
   }
 
-  allowDrop(event) {
-    console.log('allowDrop', event)
+  onDragLeave(event: DragEvent) {
+    this.testElementRef.nativeElement.style.display = 'none';
+  }
+  
+  onDragFinished(event: DragEvent) {
+    let dataTransfer = event.dataTransfer.getData('data');
     event.preventDefault();
   }
 

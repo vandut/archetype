00038f9ae6e414021efd6b4d3ba47ec4a8ac545b
Konrad Bielak <konrad.bielak@gmail.com>
2017-01-16 23:31:02 +0100

    Spacing as Input property for SpacingGridComponent

--- src/app/components/snapping-grid.component.ts
+++ src/app/components/snapping-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-snapping-grid',
@@ -6,6 +6,19 @@ import { Component, AfterViewInit, ViewChild, ElementRef, HostListener } from '@
   styles: [`canvas { width: 100%; height: 100%; }`]
 })
 export class SnappingGridComponent implements AfterViewInit {
+
+  private _spacing: number = 10;
+
+  get spacing(): number {
+    return this._spacing;
+  }
+
+  @Input()
+  set spacing(value: number) {
+    this._spacing = value;
+    this.clearCanvas();
+    this.drawGrid();
+  }
 
   @ViewChild('canvas')
   canvasElementRef: ElementRef;
@@ -39,13 +52,12 @@ export class SnappingGridComponent implements AfterViewInit {
 
     context.fillStyle = "rgba(200,200,200,1)";
 
-    let spacing = 10;
-    let columns = Math.ceil(canvas.width / spacing);
-    let rows = Math.ceil(canvas.height / spacing);
+    let columns = Math.ceil(canvas.width / this.spacing);
+    let rows = Math.ceil(canvas.height / this.spacing);
 
     for (let x = 1; x < columns; x++) {
       for (let y = 1; y < rows; y++) {
-        context.fillRect(x * spacing, y * spacing, 1, 1);
+        context.fillRect(x * this.spacing, y * this.spacing, 1, 1);
       }
     }
   }

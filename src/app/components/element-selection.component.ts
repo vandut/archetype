c5b172a7d6e41c9e6744268da948b9dc18f45b4e
Konrad Bielak <konrad.bielak@gmail.com>
2017-01-17 22:47:32 +0100

    Selection extracted to separate file

--- src/app/components/element-selection.component.ts
+++ src/app/components/element-selection.component.ts
@@ -1,93 +1,7 @@
 import { Component, ElementRef } from '@angular/core';
 import { BaseDomManipulationComponent } from './base-dom-manipulation.component';
-import { HTMLElementChmod } from '../utils/HTMLElement';
 import { PageCoordinates } from '../utils/PageCoordinates';
-
-class Selection {
-
-  private htmlElement: HTMLElement;
-
-  constructor(private target: HTMLElement,
-              private selectionDragStartListener: (d: string, c: PageCoordinates) => boolean) {
-    this.htmlElement = this.at(target.offsetLeft, target.offsetTop, target.clientWidth, target.clientHeight);
-  }
-
-  private static box(l: number, r: number, t: number, b: number, w: number, h: number) {
-    return HTMLElementChmod.fromDiv()
-      .customStyle(style => {
-        style.position = 'absolute';
-      })
-      .custom(element => {
-        element.left = l;
-        element.right = r;
-        element.top = t;
-        element.bottom = b;
-        element.width = w;
-        element.height = h;
-      });
-  }
-
-  private static borderAll(x: number, y: number, w: number, h: number, padding: number) {
-    return Selection.box(x, null, y, null, w, h)
-      .pointerEvents(false)
-      .customStyle(style => {
-        style.outline = `${padding}px dashed rgba(0, 0, 0, 1.0)`;
-        style.boxShadow = `0 0 0 ${padding}px rgba(255, 255, 255, 1.0)`;
-      })
-      .done();
-  }
-
-  private static boxSegment(handler: (e: MouseEvent) => boolean, cursor: string, l: number, r: number, t: number, b: number, w: number, h: number): HTMLElementChmod {
-    return Selection.box(l, r, t, b, w, h)
-      .pointerEvents(true)
-      .customStyle(style => {
-        style.cursor = cursor;
-      })
-      .eventListener('mousedown', handler);
-  }
-
-  private at(x: number, y: number, w: number, h: number) {
-    let oPpadding = 3;
-    let iPpadding = 3;
-    let corner = 2;
-    let rootElement = Selection.borderAll(x, y, w, h, 2);
-    rootElement.appendChild(
-      Selection.boxSegment(this.handler('all'), 'move', 0, 0, 0, 0, null, null)
-        .customStyle(style => {
-          style.background = 'repeating-linear-gradient(45deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1) 10px, rgba(255, 255, 255, 0.1) 10px, rgba(255, 255, 255, 0.1) 20px)';
-        })
-        .done()
-    );
-    rootElement.appendChild(Selection.boxSegment(this.handler('w'), 'w-resize', -oPpadding, null, 0, 0, oPpadding+iPpadding, null).done());
-    rootElement.appendChild(Selection.boxSegment(this.handler('e'), 'e-resize', null, -oPpadding, 0, 0, oPpadding+iPpadding, null).done());
-    rootElement.appendChild(Selection.boxSegment(this.handler('n'), 'n-resize', 0, 0, -oPpadding, null, null, oPpadding+iPpadding).done());
-    rootElement.appendChild(Selection.boxSegment(this.handler('s'), 's-resize', 0, 0, null, -oPpadding, null, oPpadding+iPpadding).done());
-    rootElement.appendChild(Selection.boxSegment(this.handler('nw'), 'nw-resize', -oPpadding, null, -oPpadding, null, oPpadding+corner*2, oPpadding+corner*2).done());
-    rootElement.appendChild(Selection.boxSegment(this.handler('ne'), 'ne-resize', null, -oPpadding, -oPpadding, null, oPpadding+corner*2, oPpadding+corner*2).done());
-    rootElement.appendChild(Selection.boxSegment(this.handler('sw'), 'sw-resize', -oPpadding, null, null, -oPpadding, oPpadding+corner*2, oPpadding+corner*2).done());
-    rootElement.appendChild(Selection.boxSegment(this.handler('se'), 'se-resize', null, -oPpadding, null, -oPpadding, oPpadding+corner*2, oPpadding+corner*2).done());
-    return rootElement;
-  }
-
-  private handler(direction: string) {
-    return event => {
-      event.preventDefault();
-      return this.handleSelection(direction, event);
-    }
-  }
-
-  private handleSelection(direction: string, coordinates: PageCoordinates): boolean {
-    return this.selectionDragStartListener(direction, coordinates);
-  }
-
-  attach(element: HTMLElement) {
-    element.appendChild(this.htmlElement);
-  }
-
-  remove() {
-    this.htmlElement.remove();
-  }
-}
+import { ElementSelection, ElementSelectionEventType } from './selection';
 
 @Component({
   selector: 'app-element-selection',
@@ -95,7 +9,7 @@ class Selection {
 })
 export class ElementSelectionComponent extends BaseDomManipulationComponent {
 
-  private selection: Selection = null;
+  private selection: ElementSelection = null;
 
   constructor(elementRef: ElementRef) {
     super(elementRef);
@@ -104,7 +18,7 @@ export class ElementSelectionComponent extends BaseDomManipulationComponent {
   public selectTarget(target: HTMLElement) {
     this.clearSelection();
     if (target) {
-      this.selection = new Selection(target, (d, c) => this.selectionDragStarted(d, c));
+      this.selection = new ElementSelection(target, (t, d, c) => this.selectionDragStarted(t, d, c));
       this.selection.attach(this.getNativeElement());
     }
   }
@@ -116,8 +30,9 @@ export class ElementSelectionComponent extends BaseDomManipulationComponent {
     }
   }
 
-  private selectionDragStarted(direction: string, coordinates: PageCoordinates): boolean {
-    console.log(direction, coordinates);
+  private selectionDragStarted(target: HTMLElement, type: ElementSelectionEventType, coordinates: PageCoordinates): boolean {
+    console.log(target);
+    console.log(type, coordinates);
     return true;
   }
 
---
+++ src/app/components/selection.ts
@@ -0,0 +1,98 @@
+import { PageCoordinates } from '../utils/PageCoordinates';
+import { HTMLElementChmod } from '../utils/HTMLElement';
+
+export enum ElementSelectionEventType {
+  Move,
+  Resize_N,
+  Resize_S,
+  Resize_W,
+  Resize_E,
+  Resize_NW,
+  Resize_NE,
+  Resize_SW,
+  Resize_SE
+}
+
+export declare type ElementSelectionEventHandler = (target: HTMLElement, t: ElementSelectionEventType, c: PageCoordinates) => boolean;
+
+export class ElementSelection {
+
+  private htmlElement: HTMLElement;
+
+  constructor(private target: HTMLElement,
+              private eventHandler: ElementSelectionEventHandler) {
+    this.htmlElement = this.at(target.offsetLeft, target.offsetTop, target.clientWidth, target.clientHeight);
+  }
+
+  private static box(l: number, r: number, t: number, b: number, w: number, h: number) {
+    return HTMLElementChmod.fromDiv()
+      .customStyle(style => {
+        style.position = 'absolute';
+      })
+      .custom(element => {
+        element.left = l;
+        element.right = r;
+        element.top = t;
+        element.bottom = b;
+        element.width = w;
+        element.height = h;
+      });
+  }
+
+  private static borderAll(x: number, y: number, w: number, h: number, padding: number) {
+    return ElementSelection.box(x, null, y, null, w, h)
+      .pointerEvents(false)
+      .customStyle(style => {
+        style.outline = `${padding}px dashed rgba(0, 0, 0, 1.0)`;
+        style.boxShadow = `0 0 0 ${padding}px rgba(255, 255, 255, 1.0)`;
+      })
+      .done();
+  }
+
+  private static boxSegment(handler: (e: MouseEvent) => boolean, cursor: string, l: number, r: number, t: number, b: number, w: number, h: number): HTMLElementChmod {
+    return ElementSelection.box(l, r, t, b, w, h)
+      .pointerEvents(true)
+      .customStyle(style => {
+        style.cursor = cursor;
+      })
+      .eventListener('mousedown', handler);
+  }
+
+  private at(x: number, y: number, w: number, h: number) {
+    let oPpadding = 3;
+    let iPpadding = 3;
+    let corner = 2;
+    let selectionBackground = 'repeating-linear-gradient(45deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1) 10px, rgba(255, 255, 255, 0.1) 10px, rgba(255, 255, 255, 0.1) 20px)';
+    let rootElement = ElementSelection.borderAll(x, y, w, h, 2);
+    rootElement.appendChild(ElementSelection.boxSegment(this.handler(ElementSelectionEventType.Move), 'move', 0, 0, 0, 0, null, null).customStyle(style => style.background = selectionBackground).done());
+    rootElement.appendChild(ElementSelection.boxSegment(this.handler(ElementSelectionEventType.Resize_W), 'w-resize', -oPpadding, null, 0, 0, oPpadding+iPpadding, null).done());
+    rootElement.appendChild(ElementSelection.boxSegment(this.handler(ElementSelectionEventType.Resize_E), 'e-resize', null, -oPpadding, 0, 0, oPpadding+iPpadding, null).done());
+    rootElement.appendChild(ElementSelection.boxSegment(this.handler(ElementSelectionEventType.Resize_N), 'n-resize', 0, 0, -oPpadding, null, null, oPpadding+iPpadding).done());
+    rootElement.appendChild(ElementSelection.boxSegment(this.handler(ElementSelectionEventType.Resize_S), 's-resize', 0, 0, null, -oPpadding, null, oPpadding+iPpadding).done());
+    rootElement.appendChild(ElementSelection.boxSegment(this.handler(ElementSelectionEventType.Resize_NW), 'nw-resize', -oPpadding, null, -oPpadding, null, oPpadding+corner*2, oPpadding+corner*2).done());
+    rootElement.appendChild(ElementSelection.boxSegment(this.handler(ElementSelectionEventType.Resize_NE), 'ne-resize', null, -oPpadding, -oPpadding, null, oPpadding+corner*2, oPpadding+corner*2).done());
+    rootElement.appendChild(ElementSelection.boxSegment(this.handler(ElementSelectionEventType.Resize_SW), 'sw-resize', -oPpadding, null, null, -oPpadding, oPpadding+corner*2, oPpadding+corner*2).done());
+    rootElement.appendChild(ElementSelection.boxSegment(this.handler(ElementSelectionEventType.Resize_SE), 'se-resize', null, -oPpadding, null, -oPpadding, oPpadding+corner*2, oPpadding+corner*2).done());
+    return rootElement;
+  }
+
+  private handler(type: ElementSelectionEventType) {
+    return event => {
+      event.preventDefault();
+      return this.handleSelection(type, event);
+    }
+  }
+
+  private handleSelection(type: ElementSelectionEventType, coordinates: PageCoordinates): boolean {
+    return this.eventHandler(this.htmlElement, type, coordinates);
+  }
+
+  attach(element: HTMLElement) {
+    element.appendChild(this.htmlElement);
+  }
+
+  remove() {
+    this.htmlElement.remove();
+  }
+
+}

3e912cd8bb4fd8d9b3d08551d0cb95fea3b914ab
Konrad Bielak <konrad.bielak@gmail.com>
2017-03-19 23:59:34 +0100

    Drag tree explorer initiated from root

--- src/modules/app/components/element-explorer-tree.component.ts
+++ src/modules/app/components/element-explorer-tree.component.ts
@@ -3,7 +3,6 @@ import { EditorElement } from '../services/element-repository.service';
 import { ElementSelectionService } from '../services/element-selection.service';
 import { PageCoordinatesHelper } from '../../shared/PageCoordinatesHelper';
 
-// TODO: extract as a component to separate module
 @Component({
   selector: 'app-element-explorer-tree',
   templateUrl: './element-explorer-tree.component.html',
@@ -16,49 +15,25 @@ export class ElementExplorerTreeComponent {
 
   @ViewChildren('dropZone')
   private dropZones: ElementRef[];
-  private activedropZone: ElementRef = null;
+  @ViewChildren('tree')
+  private nestedTrees: ElementExplorerTreeComponent[];
 
-  constructor(private elementSelectionService: ElementSelectionService) {}
+  constructor(public elementRef: ElementRef,
+              private elementSelectionService: ElementSelectionService) {}
 
   public onNodeClick(nodeId) {
     this.elementSelectionService.select(nodeId);
   }
 
-  @HostListener('panmove', ['$event'])
-  public onPanMove(event: HammerInput) {
-    const result = this.findDropZone(event.center);
-    this.highlightDropZone(result);
-  }
-
-  @HostListener('panend', ['$event'])
-  public onPanEnd(event: HammerInput) {
-    const result = this.findDropZone(event.center);
-    if (result) {
-      console.log(result);
-    }
-    this.highlightDropZone(null);
-  }
-
-  @HostListener('pancancel', ['$event'])
-  public onPanCancel(event: HammerInput) {
-    this.highlightDropZone(null);
-  }
-
-  private findDropZone(point: HammerPoint): ElementRef {
+  public findDropZone(point: HammerPoint): ElementRef {
     const coordinates = PageCoordinatesHelper.fromHammerPoint(point);
-    return this.dropZones.find(dropZone => PageCoordinatesHelper.isInsideElement(dropZone, coordinates));
-  }
-
-  private highlightDropZone(ref: ElementRef) {
-    if (!ref && this.activedropZone) {
-      this.activedropZone.nativeElement.dataset['show'] = 'false';
+    const zone = this.dropZones.find(dropZone => PageCoordinatesHelper.isInsideElement(dropZone, coordinates));
+    if (zone) {
+      return zone;
     }
-    if (ref && this.activedropZone && ref !== this.activedropZone) {
-      this.activedropZone.nativeElement.dataset['show'] = 'false';
-    }
-    if (ref) {
-      this.activedropZone = ref;
-      this.activedropZone.nativeElement.dataset['show'] = 'true';
+    const nestedTree = this.nestedTrees.find(tree => PageCoordinatesHelper.isInsideElement(tree.elementRef, coordinates));
+    if (nestedTree) {
+      return nestedTree.findDropZone(point);
     }
   }
 
--- src/modules/app/components/element-explorer.component.ts
+++ src/modules/app/components/element-explorer.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener, ElementRef } from '@angular/core';
 import { ElementRepositoryService } from '../services/element-repository.service';
-
+import { ElementExplorerTreeComponent } from './element-explorer-tree.component';
 
 @Component({
   selector: 'app-element-explorer',
@@ -9,6 +9,44 @@ import { ElementRepositoryService } from '../services/element-repository.service
 })
 export class ElementExplorerComponent {
 
+  @ViewChild('tree')
+  private tree: ElementExplorerTreeComponent;
+
+  private activedropZone: ElementRef = null;
+
   constructor(public elementRepositoryService: ElementRepositoryService) {}
+
+  @HostListener('panmove', ['$event'])
+  public onPanMove(event: HammerInput) {
+    const result = this.tree.findDropZone(event.center);
+    this.highlightDropZone(result);
+  }
+
+  @HostListener('panend', ['$event'])
+  public onPanEnd(event: HammerInput) {
+    const result = this.tree.findDropZone(event.center);
+    if (result) {
+      console.log(result);
+    }
+    this.highlightDropZone(null);
+  }
+
+  @HostListener('pancancel', ['$event'])
+  public onPanCancel(event: HammerInput) {
+    this.highlightDropZone(null);
+  }
+
+  private highlightDropZone(ref: ElementRef) {
+    if (!ref && this.activedropZone) {
+      this.activedropZone.nativeElement.dataset['show'] = 'false';
+    }
+    if (ref && this.activedropZone && ref !== this.activedropZone) {
+      this.activedropZone.nativeElement.dataset['show'] = 'false';
+    }
+    if (ref) {
+      this.activedropZone = ref;
+      this.activedropZone.nativeElement.dataset['show'] = 'true';
+    }
+  }
 
 }

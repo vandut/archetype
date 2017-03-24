4291e4a6264b886486a323a50bb796805a8d402d
Konrad Bielak <konrad.bielak@gmail.com>
2017-03-24 16:15:26 +0100

    Fixed problem after Angular upgrade

--- src/modules/app/components/palette/palette.component.ts
+++ src/modules/app/components/palette/palette.component.ts
@@ -13,7 +13,7 @@ export class PaletteComponent {
 
   public static DEFAULT_DIV_TEMPLATE = '<div style="width: 100px; height: 100px; background: red;">A</div>';
 
-  private items = [
+  public items = [
     {
       name: 'Stub element',
       template: PaletteComponent.DEFAULT_DIV_TEMPLATE
